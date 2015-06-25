6ca09d20ed343702a353c222de4f57df2c09ec2b
Matthew Hanson <matt.a.hanson@gmail.com>
2015-06-25 00:19:33 -0400

    placeholder script for creating new site

---
+++ geosites/addsite.py
@@ -0,0 +1,17 @@
+# script for adding new site
+
+# add site to django database, get SITE_ID
+
+
+# copy site_template folder
+
+
+# generate SECRET_KEY, modify settings file, wsgi
+
+
+# modify nginx and gunicorn configs
+
+
+# link configs, reload nginx, restart gunicorn
+
+
