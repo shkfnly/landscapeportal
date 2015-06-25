9eff7ac54b6c85d9e1a6003f28529826f8aba276
Matthew Hanson <matt.a.hanson@gmail.com>
2015-06-25 00:24:47 -0400

    added master site

---
+++ landscapeportal/site1/settings.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+###############################################
+# Geosite settings
+###############################################
+
+import os
+import geosites
+
+# Directory of master site - for GeoSites it's two up
+GEOSITES_ROOT = os.path.dirname(geosites.__file__)
+SITE_ROOT = os.path.dirname(__file__)
+
+# Read in GeoSites pre_settings
+execfile(os.path.join(GEOSITES_ROOT, 'pre_settings.py'))
+
+SITE_ID = 1
+SITE_NAME = 'GeoSite%s' % SITE_ID
+SITEURL = 'http://site1.landscapeportaldev'
+# Should be unique for each site
+SECRET_KEY = "fbk3CC3N6jt1AU9mGIcI"
+
+# globally installed apps
+SITE_APPS = ('geosites',)
+
+# Site specific databases
+SITE_DATABASES = {}
+
+##### Overrides
+# Below are some common GeoNode settings that might be overridden for site
+
+# base urls for all sites
+#ROOT_URLCONF = 'geonode.urls'
+
+# admin email
+#THEME_ACCOUNT_CONTACT_EMAIL = ''
+
+# Have GeoServer use this database for this site
+#DATASTORE = ''
+
+# Allow users to register
+#REGISTRATION_OPEN = True
+
+
+# Read in GeoSites post_settings
+execfile(os.path.join(GEOSITES_ROOT, 'post_settings.py'))
