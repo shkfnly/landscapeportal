171ea5d59c5ebd23a8c64c2d87b83c3a97fa0788
Matthew Hanson <matt.a.hanson@gmail.com>
2015-06-25 00:25:07 -0400

    updated manage to point to master site

--- manage.py
+++ manage.py
@@ -7,6 +7,6 @@ import sys
 if __name__ == "__main__":
     from django.core.management import execute_from_command_line
 
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "landscapeportal.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "landscapeportal.site1.settings")
     execute_from_command_line(sys.argv)
 
