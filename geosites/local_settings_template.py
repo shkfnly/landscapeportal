108d01c39268a42aae603b2e19f988ca16d0538a
Matthew Hanson <matt.a.hanson@gmail.com>
2015-07-01 18:36:40 -0400

    added local_settings templates and logic for reading in

---
+++ geosites/local_settings_template.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+###############################################
+# Geosite master local_settings
+###############################################
+
+# share a database
+"""
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': '',
+        'USER' : '',
+        'PASSWORD' : '',
+        'HOST' : '',
+        'PORT' : '',
+    }
+    # vector datastore for uploads
+    # 'datastore' : {
+    #    'ENGINE': 'django.contrib.gis.db.backends.postgis',
+    #    'NAME': '',
+    #    'USER' : '',
+    #    'PASSWORD' : '',
+    #    'HOST' : '',
+    #    'PORT' : '',
+    # }
+}
+"""
---
+++ geosites/site_template/local_settings_template.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+###############################################
+# Geosite local settings
+###############################################
+
+# load local_settings from master
+
+try:
+    # load local_settings from base project directory
+    execfile(os.path.join(SITE_ROOT, '../', local_settings.py'))
+except:
+    # there are no master local_settings to import
+    pass
+
+# Outside URL
+#SITEURL = 'http://www.geonode.org'
+
+# databases unique to site if not defined in site settings
+"""
+SITE_DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(PROJECT_ROOT, '../development.db'),
+    },
+}
+"""
+
