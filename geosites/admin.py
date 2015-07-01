4b46c8505e250c5434a5576a2e1eebd94c4576e3
Simone Dalmasso <simone.dalmasso@gmail.com>
2015-07-01 14:41:06 +0200

    add list display and make site readonly from within the admin detail page

--- geosites/admin.py
+++ geosites/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import SiteResources
 
-admin.site.register(SiteResources)
+
+class SiteResourceAdmin(admin.ModelAdmin):
+    filter_horizontal = ('resources',)
+    readonly_fields = ('site',)
+
+admin.site.register(SiteResources, SiteResourceAdmin)
