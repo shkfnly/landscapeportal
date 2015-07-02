3ac3b654598920564ede60c49f1c8b100f817e6a
Matthew Hanson <matt.a.hanson@gmail.com>
2015-07-02 03:35:22 +0200

    added manage script to recursively run on all sites

---
+++ manage_all.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import glob
+
+PROJECT = 'landscapeportal'
+
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+
+    print os.listdir(PROJECT)
+    sites = [os.path.basename(d) for d in os.listdir(PROJECT) if os.path.isdir(os.path.join(PROJECT,d))]
+    print sites
+    for site in sites:
+	print 'Managing %s' % site
+	path = '%s/%s/settings.py' % (PROJECT, site)
+	if os.path.exists(path):
+    	    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "%s.%s.settings" % (PROJECT, site))
+    	    execute_from_command_line(sys.argv)
+
