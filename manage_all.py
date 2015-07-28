43eae87ec1aa2a5dfeed79a07b9a9106e94dca20
Matthew Hanson <matt.a.hanson@gmail.com>
2015-07-28 15:26:29 +0200

    fixed manage_all to only run only on site folders

--- manage_all.py
+++ manage_all.py
@@ -9,7 +9,7 @@ PROJECT = 'landscapeportal'
 if __name__ == "__main__":
     from django.core.management import execute_from_command_line
 
-    sites = [os.path.basename(d) for d in os.listdir(PROJECT) if os.path.isdir(os.path.join(PROJECT,d))]
+    sites = [os.path.basename(d) for d in os.listdir(PROJECT) if os.path.isfile(os.path.join(PROJECT,d,'settings.py'))]
     for site in sites:
 	print 'Managing %s' % site
 	path = '%s/%s/settings.py' % (PROJECT, site)
